0f766fb1eb22c58389ce189d688c0483135125b0
marcelo <marcelochanquia@hotmail.com>
2019-04-03 03:57:33 -0300

    Version 3

--- TodoAPI/TodoAPI/Models/TodoItem.cs
+++ TodoAPI/TodoAPI/Models/TodoItem.cs
@@ -7,7 +7,7 @@ namespace TodoAPI.Models
 {
     public class TodoItem
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
     }
